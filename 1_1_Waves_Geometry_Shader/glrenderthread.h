5290f4a8da791088d95fb805a3b49d9b9c83382b
rakesh-malviya <rakesh686111@gmail.com>
2014-06-14 18:19:08 +0530

    LICENSE.txt added

---
+++ 1_1_Waves_Geometry_Shader/glrenderthread.h
@@ -0,0 +1,48 @@
+#ifndef GLRENDERTHREAD_H
+#define GLRENDERTHREAD_H
+
+#include <QThread>
+#include <QGLWidget>
+
+class QGLFrame;
+class QSize;
+class QGLShaderProgram;
+class QGLShader;
+
+class QGLRenderThread : public QThread
+{
+    Q_OBJECT
+public:
+    explicit QGLRenderThread(QGLFrame *parent = 0);
+    void resizeViewport(const QSize &size);
+    void run(void);
+    void stop(void);
+    void LoadShader(QString vshader, QString fshader);
+    void LoadShader(QString vshader, QString fshader,QString gshader);
+    void paintGL(void);
+    int xRot;
+    int yRot;
+    int zRot;
+    float timerCount;
+protected:
+    void GLInit(void);
+    void GLResize(int width, int height);
+
+
+private:
+    bool doRendering, doResize;
+    int w, h, FrameCounter;
+
+    QGLFrame *GLFrame;
+
+    QGLShaderProgram *ShaderProgram;
+    QGLShader *VertexShader, *FragmentShader;
+    QGLShader *GeometryShader;
+
+signals:
+
+public slots:
+
+};
+
+#endif // GLRENDERTHREAD_H
